b658c41642a5e0696694a6693f7bfcd74ed8fe07
sobik.sebastian@gmail.com <sobik.sebastian@gmail.com>
2022-12-08 13:33:14 +0100

    invalideQueries done

--- front-end/src/components/Rentals.tsx
+++ front-end/src/components/Rentals.tsx
@@ -1,9 +1,9 @@
-import {QueryClient, useQuery, useMutation} from 'react-query';
+import {useQueryClient, useQuery, useMutation} from 'react-query';
 import RentalDataService from '../services/rental.service';
 
-const queryClient = new QueryClient()
-
 const Rentals = () => {
+
+  const queryClient = useQueryClient();
 
   const query = useQuery({ queryKey: ['rentals'], queryFn: RentalDataService.getAll });
 
@@ -18,14 +18,14 @@ const Rentals = () => {
       <div>
       <ul>
         {query.data?.data.map(rental => (
-          <li key={rental.id}>{rental.title}</li>
+          <li key={rental._id}>{rental.title}</li>
         ))}
       </ul>
 
       <button
         onClick={() => {
           mutation.mutate({
-            id: 1,
+            _id: 1,
             description: "rental car",
             title: 'Sixt',
           })
--- front-end/src/types/rental.type.ts
+++ front-end/src/types/rental.type.ts
@@ -1,5 +1,5 @@
 export default interface IRentalData {
-    id?: any | null,
+    _id?: any | null,
     title: string,
     description: string,
   }
