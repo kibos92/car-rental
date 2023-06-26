45801a62e9c862b6ef5d1b54aa9dea268ff26982
sobik.sebastian@gmail.com <sobik.sebastian@gmail.com>
2023-06-26 22:56:53 +0200

    little fix

--- front-end/src/components/Reservations.tsx
+++ front-end/src/components/Reservations.tsx
@@ -1,9 +1,12 @@
 import { useQuery} from 'react-query';
 import ReservationDataService from '../services/reservation.service';
+import CarDataService from '../services/car.service';
 
 const Reservations = () => {
   
   const getAll = useQuery({ queryKey: ['reservations'], queryFn: ReservationDataService.getAll });
+
+  const getAllCars = useQuery('cars', CarDataService.getAllCars);
 
     return (
       <div>
@@ -12,17 +15,20 @@ const Reservations = () => {
   
         <h1>Lista rezerwacji:</h1>
         <div className='block'>
-      <ul>
-        {getAll.data?.data.map(reservation => (
-          <li className='block' key={reservation._id}>
+        <ul>
+        {getAll.data?.data.map((reservation) => {
+          const car = getAllCars.data?.data.find((car) => car._id === reservation.carId);
+          const carName = car ? `${car.brand} ${car.model}` : '';
 
-            CarID: {reservation.carId},
-            UserID: {reservation.userId},
-            StartDate: {reservation.startDate},
-            EndDate: {reservation.endDate}
-            
+          return (
+            <li className='block' key={reservation._id}>
+              UserID: {reservation.userId}, 
+              StartDate: {reservation.startDate.slice(0, 10)}, 
+              EndDate: {reservation.endDate.slice(0, 10)}, 
+              Car: {carName}
             </li>
-        ))}
+          );
+        })}
       </ul>
       </div>
         </div>
